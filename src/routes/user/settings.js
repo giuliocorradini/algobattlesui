d184be43c083f1af62f152abf5265ae8fb4df479
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-08-19 22:50:48 +0200

    Fixed header

--- src/routes/user/settings.js
+++ src/routes/user/settings.js
@@ -32,16 +32,12 @@ function NonBlankInput({error, defaultValue, errorLabel, ...props}) {
 }
 
 function Header({user}) {
-    return <header className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
+    return <header className="sticky top-0 z-10 flex h-14 items-center justify-between border-b bg-white px-4 md:px-6">
         <Link to="/" className="flex items-center gap-2 font-bold">
             <SwordsIcon className="h-6 w-6" />
             AlgoBattles
         </Link>
-        <nav className="hidden font-medium sm:flex flex-row items-center gap-5 text-sm lg:gap-6">
-        </nav>
-        <div className="flex items-center w-full gap-4 md:ml-auto md:gap-2 lg:gap-4">
-            <AccountButton username={user.username} email={user.email} picture={user.picture}></AccountButton>
-        </div>
+        <AccountButton username={user.username} email={user.email} picture={user.picture}></AccountButton>
     </header>
 }
 
