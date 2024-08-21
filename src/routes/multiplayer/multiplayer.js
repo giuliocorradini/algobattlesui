369c6f562dc498c5705e5f3c30c38cd23af4d65f
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-08-21 16:40:57 +0200

    Parse and list users

--- src/routes/multiplayer/multiplayer.js
+++ src/routes/multiplayer/multiplayer.js
@@ -1,10 +1,28 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import useWebSocket from "react-use-websocket";
 import { AuthenticationContext } from "../../lib/api";
+
+function Member({id, username, first_name, last_name}) {
+    return <div>
+        {username}, with ID {id}. {first_name} {last_name}
+    </div>
+}
 
 export default function MultiplayerPage() {
     const { isLogged, token } = useContext(AuthenticationContext)
     const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`ws://localhost:8000/ws/multiplayer?token=${token}`);
+    const [members, setMembers] = useState([])
+    
+    function decodeLastJsonEffect() {
+        if (!lastJsonMessage)
+            return
+        
+        if ("members" in lastJsonMessage) {
+            setMembers(lastJsonMessage.members)
+        }
+    }
+
+    useEffect(decodeLastJsonEffect, [lastJsonMessage])
 
     return <div>
         Multiplayer
@@ -14,5 +32,10 @@ export default function MultiplayerPage() {
         <button onClick={() => {sendJsonMessage({
             message: "west end girls"
         })}}>Send message</button>
+
+        <p>Lista utenti</p>
+        {
+            members.map((m, i) => <Member key={i} {...m}/>)
+        }
     </div>
 }
